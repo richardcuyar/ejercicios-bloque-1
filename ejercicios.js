8eb70d38c71fc4320904b3de49a5ee7a2b824c50
richardcuyar <richardcuyar@gmail.com>
2024-09-23 19:54:25 +0200

    Add files via upload

---
+++ ejercicios.js
@@ -0,0 +1,94 @@
+// EJERCICIO 1
+
+let myFavoriteHero = "Hulk";
+let x = 50;
+let h = 5;
+let y = 10;
+let z = h + y;
+/*console.log(z)*/
+
+
+// EJERCICIOS 2
+
+
+/*1.2 Declara 3 variables con los nombres y valores siguientes 
+	firstName = 'Jon'; 
+	lastName = 'Snow'; 
+	age = 24; 
+Muestralos por consola de esta forma: 
+	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
+
+1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
+ambos juguetes.
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+/
+1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
+y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
+basePrice más el valor de la variable globalBasePrice.
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+const character = {name: 'Jack Sparrow', age: "10"};
+delete character.age;
+character.age = 25
+console.log(character);  { name: 'Jack Sparrow', age: 25 } */
+
+let firstName = "Jon";
+let lastName = "Snow";
+let age = 24;
+let frase = 'Soy ${firstName} ${lastName}, tengo ${24} años y me gustan los lobos.'
+console.log (frase);
+
+
+
+
+/* // EJERCICIO 3
+/* 
+let b = h * y;
+/*console.log(b)  --- 50 */  
+let f = y / 2;
+/*console.log(f)   --- 5 */
+let d = 15 % 9;
+/*console.log(d)  --- 6 */
+let p = 10;
+let j = 5;
+let o = 10 + 5;
+/*console.log(o)   --- 15*/
+let c = 10;
+let m = 5;
+let i = c * m;
+/*console.log(i)   --- 50*/
+
+
+/// EJERCICIO 4
+
+/* const avengers = ["HULK", "SPIDERMAN","BLACK PANTHER"];
+const menosAvengers = avengers.shift();
+console.log(avengers);
+console.log(menosAvengers);  --- "HULK" */ 
+
+/* const avengers = ["HULK", "SPIDERMAN","BLACK PANTHER"];
+console.log (avengers.unshift("IRONMAN"));  --- 4
+console.log(avengers);  --- [ 'IRONMAN', 'HULK', 'SPIDERMAN', 'BLACK PANTHER' ] */
+
+/* const avengers = ["HULK", "SPIDERMAN","BLACK PANTHER"];
+console.log (avengers.unshift());  --- 3 */
+
+/* const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+console.log(rickAndMortyCharacters);  --- [ 'Rick', 'Beth', 'Jerry', 'Morty', 'Summer' ]
+console.log (rickAndMortyCharacters.pop());  --- Summer */
+
+/*const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+const ultimoRickAndMortyCharacters = rickAndMortyCharacters.pop();
+console.log(rickAndMortyCharacters);  --- [ 'Rick', 'Beth', 'Jerry', 'Morty', 'Summer' ] 
+console.log(rickAndMortyCharacters.shift());  --- Rick
+console.log(rickAndMortyCharacters.pop());  --- Summer */
+
+/*const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+const MenosRickAndMortyCharacters = rickAndMortyCharacters.splice(1,1,);
+console.log(rickAndMortyCharacters);  --- [ 'Rick', 'Jerry', 'Morty', 'Summer', 'Lapiz Lopez' ]*/
+
+
